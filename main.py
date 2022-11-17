49dc89491817b99da7e762dd73a5b05f6b56551d
v1.vedernikov <alexander.vedernikov@viableone.com>
2022-11-17 13:21:27 +0100

    copy

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import logging
+from datetime import datetime
+
+logging.getLogger("werkzeug").disabled = True
+logging.basicConfig(filename='record.log', level=logging.DEBUG)
+
+
+app = Flask(__name__)
+
+now = ""
+@app.route('/')
+def main():
+    now = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+" ")
+    app.logger.debug( now + "successfully loaded index.html for " + str(request.remote_addr))
+    return render_template('index.html')
+
+
+@app.route('/feedback/')
+def form():
+    now = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+" ")
+    app.logger.debug(now + "successfully loaded form.html for " + str(request.remote_addr))
+    return render_template('form.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)
